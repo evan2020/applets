457034bbb06ef1e7fd2384f122f7e3d11e401305
evan2020 <evan199101@gmail.com>
2018-05-13 13:17:31 +0800

    删除一些多余资源

--- dist/_wepylogs.js
+++ dist/_wepylogs.js
@@ -1,4 +1,1 @@
 console.log('WePY开启错误监控');
-console.warn(`CLI报错：WARNING IN : src/appletsA/pages/details/520.wpy
-[xmldom warning]	attribute "“text”" missed quot(")!!
-@#[line:undefined,col:undefined]`);
